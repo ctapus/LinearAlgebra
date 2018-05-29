f1e17a2cd11f6d3fc8c3b8df1f13c0ec90364e48
ctapus <ctapus@hotmail.com>
2018-05-29 15:51:14 +0200

    VectorSpacePresenter

--- presenters/SystemOfLinearEquationsPresenter.ts
+++ presenters/SystemOfLinearEquationsPresenter.ts
@@ -2,24 +2,24 @@
 
 export class SystemOfLinearEquationsPresenter {
 	public static printTableSystem(systemOfEquations: SystemOfLinearEquations, container: JQuery): void {
-		let table0: JQuery = $("<table></table>");
-		let tr0: JQuery = $("<tr></tr>");
+		const table0: JQuery = $("<table></table>");
+		const tr0: JQuery = $("<tr></tr>");
 		table0.append(tr0);
-		let td01: JQuery = $("<td></td>");
+		const td01: JQuery = $("<td></td>");
 		tr0.append(td01);
-		let td02: JQuery = $("<td></td>").text("*").css("vertical-align", "middle").css("padding-left", "5px").css("padding-right", "5px");
+		const td02: JQuery = $("<td></td>").text("*").css("vertical-align", "middle").css("padding-left", "5px").css("padding-right", "5px");
 		tr0.append(td02);
-		let td03: JQuery = $("<td></td>");
+		const td03: JQuery = $("<td></td>");
 		tr0.append(td03);
-		let td04: JQuery = $("<td></td>").text("=").css("vertical-align", "middle").css("padding-left", "5px").css("padding-right", "5px");
+		const td04: JQuery = $("<td></td>").text("=").css("vertical-align", "middle").css("padding-left", "5px").css("padding-right", "5px");
 		tr0.append(td04);
-		let td05: JQuery = $("<td></td>");
+		const td05: JQuery = $("<td></td>");
 		tr0.append(td05);
 		let table: JQuery = $("<table></table>").addClass("matrix");
 		for (let i: number = 0; i < systemOfEquations.A.m; i++) {
-			let tr: JQuery = $("<tr></tr>");
+			const tr: JQuery = $("<tr></tr>");
 			for (let j: number = 0; j < systemOfEquations.A.n; j++) {
-				let td: JQuery = $("<td></td>").text(systemOfEquations.A.elements[i][j].toString()).css("padding-left", "5px")
+				const td: JQuery = $("<td></td>").text(systemOfEquations.A.elements[i][j].toString()).css("padding-left", "5px")
 				.css("padding-right", "5px");
 				tr.append(td);
 			}
@@ -28,9 +28,9 @@ export class SystemOfLinearEquationsPresenter {
 		td01.append(table);
 		table = $("<table></table>").addClass("matrix");
 		for (let j: number = 0; j < systemOfEquations.noVariables; j++) {
-			let tr: JQuery = $("<tr></tr>");
-			let td: JQuery = $("<td></td>").text("x").css("padding-left", "5px").css("padding-right", "5px");
-			let sub: JQuery = $("<sub></sub>").text(j.toString());
+			const tr: JQuery = $("<tr></tr>");
+			const td: JQuery = $("<td></td>").text("x").css("padding-left", "5px").css("padding-right", "5px");
+			const sub: JQuery = $("<sub></sub>").text(j.toString());
 			td.append(sub);
 			tr.append(td);
 			table.append(tr);
@@ -38,8 +38,8 @@ export class SystemOfLinearEquationsPresenter {
 		td03.append(table);
 		table = $("<table></table>").addClass("matrix");
 		for (let j: number = 0; j < systemOfEquations.b.m; j++) {
-			let tr: JQuery = $("<tr></tr>");
-			let td: JQuery = $("<td></td>").text(systemOfEquations.b.elements[j].toString()).css("padding-left", "5px").css("padding-right", "5px");
+			const tr: JQuery = $("<tr></tr>");
+			const td: JQuery = $("<td></td>").text(systemOfEquations.b.elements[j].toString()).css("padding-left", "5px").css("padding-right", "5px");
 			tr.append(td);
 			table.append(tr);
 		}
--- presenters/VectorSpacePresenter.ts
+++ presenters/VectorSpacePresenter.ts
@@ -4,17 +4,27 @@ import { VectorSpace } from "../structures/VectorSpace";
 
 export class VectorSpacePresenter {
     public static printVectorSpace(vectorSpace: VectorSpace, container: JQuery): void {
-        const table: JQuery = $("<table></table>").addClass("matrix");
-        container.append(table);
-        container.append("<br />");
+        const tableMain: JQuery = $("<table></table>");
+        container.append(tableMain);
+        const trMain: JQuery = $("<tr></tr>");
+        tableMain.append(trMain);
         for (let i: number = 0; i < vectorSpace.m; i++) {
-            const tr: JQuery = $("<tr></tr>");
+            const tdMain: JQuery = $("<td></td>");
+            trMain.append(tdMain);
+            if (i !== vectorSpace.m - 1) {
+                const td2Main: JQuery = $("<td></td>").text(",").css("vertical-align", "bottom").css("padding-left", "5px").css("padding-right", "5px");
+                trMain.append(td2Main);
+            }
+            const table: JQuery = $("<table></table>").addClass("matrix");
+            tdMain.append(table);
             const vector: Vector = vectorSpace.elements[i];
             for (let j: number = 0; j < vector.m; j++) {
+                const tr: JQuery = $("<tr></tr>");
                 const td: JQuery = $("<td></td>").text(vector.elements[j].toString()).addClass("matrixElement");
                 tr.append(td);
+                table.append(tr);
             }
-            table.append(tr);
         }
+        container.append("<br />");
     }
 }
