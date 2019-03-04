b023ca5d007464828acee6fd820fe00344ba4e44
ctapus <ctapus@hotmail.com>
2019-03-04 23:06:39 +0100

    fixed probability calculation

--- src/exercises/decisionTree.ts
+++ src/exercises/decisionTree.ts
@@ -47,13 +47,13 @@ $(document).ready(() => {
     let giniRow: string = "";
     for (let i: number = 0; i < dataset[0].length; i++) {
         const giniImpurity: number = DecisionTree.calculateGiniImpurity(DecisionTree.getColumnFromArray(dataset, i));
-        giniRow += `<td>${giniImpurity}</td>`;
+        giniRow += `<td>${giniImpurity.toFixed(2)}</td>`;
     }
     $("#tblDataset").append($(`<tr>${giniRow}</tr>`));
     let entropyRow: string = "";
     for (let i: number = 0; i < dataset[0].length; i++) {
         const entropy: number = DecisionTree.calculateEntropy(DecisionTree.getColumnFromArray(dataset, i));
-        entropyRow += `<td>${entropy}</td>`;
+        entropyRow += `<td>${entropy.toFixed(2)}</td>`;
     }
     $("#tblDataset").append($(`<tr>${entropyRow}</tr>`));
 });
@@ -81,34 +81,35 @@ class DecisionTree {
         
     }
 
-    public static calculateGiniImpurity(feature: Feature): number {
+    private static getClassesProbability(feature: Feature): { [key: string]: number } {
         const dictionary: { [key: string]: number } = {};
         for (const element of feature) {
             if (!dictionary.hasOwnProperty(element)) {
-                dictionary[element] = 0;
+                dictionary[element] = 1;
             }
             else {
                 dictionary[element] ++;
             }
         }
-        let gini: number = 1;
+        return dictionary;
+    }
+    public static calculateGiniImpurity(feature: Feature): number {
+        /**
+         * @remarks
+         * Gini impurity is a measure of how often a randomly chosen element from the set would be incorrectly labeled
+         * if it was randomly labeled according to the distribution of labels in the subset
+         */
+        const dictionary: { [key: string]: number } = this.getClassesProbability(feature);
+        let giniImpurity: number = 1;
         for (const key of Object.keys(dictionary)) {
             const value: number = dictionary[key];
             const pi: number = value/feature.length;
-            gini -= Math.pow(pi, 2);
+            giniImpurity -= Math.pow(pi, 2);
         }
-        return gini;
+        return giniImpurity;
     }
     public static calculateEntropy(feature: Feature): number {
-        const dictionary: { [key: string]: number } = {};
-        for (const element of feature) {
-            if (!dictionary.hasOwnProperty(element)) {
-                dictionary[element] = 0;
-            }
-            else {
-                dictionary[element] ++;
-            }
-        }
+        const dictionary: { [key: string]: number } = this.getClassesProbability(feature);
         let entropy: number = 0;
         for (const key of Object.keys(dictionary)) {
             const value: number = dictionary[key];
