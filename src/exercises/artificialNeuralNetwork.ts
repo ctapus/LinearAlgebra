229c217c3b885aa8e8324ac39bbbb834eaeb89c4
ctapus <ctapus@hotmail.com>
2019-08-18 23:27:57 +0200

    ann wip

--- src/exercises/artificialNeuralNetwork.ts
+++ src/exercises/artificialNeuralNetwork.ts
@@ -1,57 +1,93 @@
+/* tslint:disable:max-classes-per-file eofline*/
 class Dendrite {
-    public weight: number = Math.random();
+	public weight: number;
+	public neuron: Neuron;
+	public constructor(neuron: Neuron) {
+		this.weight = Math.random();
+		this.neuron = neuron;
+	}
 }
 class Neuron {
-    public dendrites: Dendrite[];
-    public bias: number = Math.random() - 0.5;
-    public value: number;
-    public constructor(numberOfDendrites: number) {
-        this.dendrites = new Array<Dendrite>(numberOfDendrites); // number of dendrites is equal to the number of neurons in the previous layer
-        for(let i: number = 0; i < numberOfDendrites; i++) {
-            this.dendrites[i] = new Dendrite();
-        }
-    }
+	public dendrites: Dendrite[];
+	public bias: number = Math.random() - 0.5;
+	public value: number;
+	public constructor(previousLayer: Layer) {
+		if (previousLayer === null) {
+			this.dendrites = new Array<Dendrite>();
+			return;
+		}
+		this.dendrites = new Array<Dendrite>(previousLayer.neurons.length); // number of dendrites is equal to the number of neurons in the previous layer
+		for (let i: number = 0; i < previousLayer.neurons.length; i++) {
+			this.dendrites[i] = new Dendrite(previousLayer.neurons[i]);
+		}
+	}
+	public calculate(): void {
+		if (this.dendrites.length === 0) {
+			return;
+		}
+		this.value = this.activate(this.bias + this.dendrites.map((x) => x.weight * x.neuron.value).reduce((acc, current) => acc + current));
+	}
+	private activate(value: number): number {
+		return Math.tanh(value);
+	}
 }
 class Layer {
-    public neurons: Neuron[];
-    public constructor(numberOfNeurons: number, previousLayerNumberOfNeurons: number) {
-        this.neurons = new Array<Neuron>(numberOfNeurons);
-        for(let i: number = 0; i < numberOfNeurons; i++) {
-            this.neurons[i] = new Neuron(previousLayerNumberOfNeurons);
-        }
-    }
+	public neurons: Neuron[];
+	public constructor(numberOfNeurons: number, previousLayer: Layer) {
+		this.neurons = new Array<Neuron>(numberOfNeurons);
+		for (let i: number = 0; i < numberOfNeurons; i++) {
+			this.neurons[i] = new Neuron(previousLayer);
+		}
+	}
+	public calculate(): void {
+		for (const neuron of this.neurons) {
+			neuron.calculate();
+		}
+	}
 }
 class Network {
-    public layers: Layer[];
+	public layers: Layer[];
+	public fitness: number;
 	constructor(n: number | number[]);
 	constructor(n: any) {
 		if (typeof n === "number") {
 			this.layers = new Array<Layer>(n);
 		} else if (n instanceof Array) {
 			this.layers = new Array<Layer>(n.length);
-            this.layers[0] = new Layer(n[0], 0);
+			this.layers[0] = new Layer(n[0], null);
 			for (let i: number = 1; i < n.length; i++) {
-				this.layers[i] = new Layer(n[i], n[i-1]);
+				this.layers[i] = new Layer(n[i], this.layers[i - 1]);
 			}
+		}
+	}
+	public feedForward(): void {
+		for (const layer of this.layers) {
+			layer.calculate();
 		}
 	}
 }
 
 $(document).ready(() => {
 
-    const network: Network = new Network([5, 4, 3]); // 3 layers with 5, 4 and 3 neurons respectively
-    const div: JQuery = $("#divArtificialNeuralNetwork");
-    for (const layer of network.layers) {
-        const table: JQuery = $("<table  style='float: left; margin: 5px;'></table>");
-        div.append(table);
-        for (const neuron of layer.neurons) {
-            const tr: JQuery = $("<tr></tr>");
-            table.append(tr);
-            const td: JQuery = $("<td></td>");
-            tr.append(td);
-            const span: JQuery = $("<span></span>");
-            td.append(span);
-            span.append("B: " + neuron.bias + ". D: [" + neuron.dendrites.map((value) => { value.weight.toString(); }).join("; ") + "]");
-        }
-    }
+		const network: Network = new Network([5, 4, 3]); // 3 layers with 5, 4 and 3 neurons respectively
+		network.layers[0].neurons[0].value = 1;
+		network.layers[0].neurons[1].value = .123;
+		network.layers[0].neurons[2].value = 1.12;
+		network.layers[0].neurons[3].value = 1.56;
+		network.layers[0].neurons[4].value = .89;
+		network.feedForward();
+		const div: JQuery = $("#divArtificialNeuralNetwork");
+		for (const layer of network.layers) {
+				const table: JQuery = $("<table  style='float: left; margin: 5px;'></table>");
+				div.append(table);
+				for (const neuron of layer.neurons) {
+						const tr: JQuery = $("<tr></tr>");
+						table.append(tr);
+						const td: JQuery = $("<td></td>");
+						tr.append(td);
+						const span: JQuery = $("<span></span>");
+						td.append(span);
+						span.append("<strong>V: " + neuron.value + "</strong><br/> B: " + neuron.bias + "<br/> D: [" + neuron.dendrites.map((x) => x.weight.toString()).join("; ") + "]");
+				}
+		}
 });
--- src/exercises/decisionTree.ts
+++ src/exercises/decisionTree.ts
@@ -1,50 +1,50 @@
 
-import { DecisionTree, Leaf, Feature } from "../structures/DecisionTree";
+import { DecisionTree, Feature, Leaf } from "../structures/DecisionTree";
 // DataSet
 const dataset: Feature[] = [
-    ["DK", "January", "February", 58, "M", "A" ],
-    ["ES", "January", "January", 71, "F", "A" ],
-    ["ES", "February", "March", 69, "M", "A" ],
-    ["AE", "February", "February", 60, "M", "D" ],
-    ["DO", "December", "January", 54, "F", "A" ],
-    ["ES", "January", "January", 66, "M", "A" ],
-    ["AE", "February", "March", 66, "F", "A" ],
-    ["TH", "February", "February", 56, "M", "A" ],
-    ["TH", "February", "March", 56, "M", "A" ],
-    ["IT", "December", "January", 67, "M", "D" ],
-    ["IT", "January", "January", 90, "M", "D" ],
-    ["NL", "December", "December", 70, "F", "A" ],
-    ["NL", "December", "January", 70, "F", "A" ],
-    ["ZA", "January", "January", 69, "F", "A" ],
-    ["ZA", "January", "January", 69, "F", "A" ],
-    ["ZA", "January", "January", 69, "F", "A" ],
-    ["ZA", "January", "January", 69, "F", "A" ],
-    ["ZA", "January", "January", 69, "F", "A" ],
-    ["ZA", "January", "January", 69, "F", "A" ],
-    ["ZA", "January", "January", 51, "F", "A" ],
-    ["ZA", "January", "January", 51, "F", "A" ],
-    ["ZA", "January", "January", 51, "F", "A" ],
-    ["ZA", "January", "January", 51, "F", "A" ],
-    ["ZA", "January", "January", 51, "F", "A" ],
-    ["NL", "February", "February", 57, "F", "A" ],
-    ["MX", "January", "February", 59, "F", "A" ],
-    ["DE", "January", "January", 80, "M", "A" ],
-    ["IT", "March", "March", 70, "M", "A" ],
-    ["ES", "February", "February", 43, "M", "A" ],
-    ["SR", "March", "March", 57, "M", "D" ],
+	["DK", "January", "February", 58, "M", "A" ],
+	["ES", "January", "January", 71, "F", "A" ],
+	["ES", "February", "March", 69, "M", "A" ],
+	["AE", "February", "February", 60, "M", "D" ],
+	["DO", "December", "January", 54, "F", "A" ],
+	["ES", "January", "January", 66, "M", "A" ],
+	["AE", "February", "March", 66, "F", "A" ],
+	["TH", "February", "February", 56, "M", "A" ],
+	["TH", "February", "March", 56, "M", "A" ],
+	["IT", "December", "January", 67, "M", "D" ],
+	["IT", "January", "January", 90, "M", "D" ],
+	["NL", "December", "December", 70, "F", "A" ],
+	["NL", "December", "January", 70, "F", "A" ],
+	["ZA", "January", "January", 69, "F", "A" ],
+	["ZA", "January", "January", 69, "F", "A" ],
+	["ZA", "January", "January", 69, "F", "A" ],
+	["ZA", "January", "January", 69, "F", "A" ],
+	["ZA", "January", "January", 69, "F", "A" ],
+	["ZA", "January", "January", 69, "F", "A" ],
+	["ZA", "January", "January", 51, "F", "A" ],
+	["ZA", "January", "January", 51, "F", "A" ],
+	["ZA", "January", "January", 51, "F", "A" ],
+	["ZA", "January", "January", 51, "F", "A" ],
+	["ZA", "January", "January", 51, "F", "A" ],
+	["NL", "February", "February", 57, "F", "A" ],
+	["MX", "January", "February", 59, "F", "A" ],
+	["DE", "January", "January", 80, "M", "A" ],
+	["IT", "March", "March", 70, "M", "A" ],
+	["ES", "February", "February", 43, "M", "A" ],
+	["SR", "March", "March", 57, "M", "D" ],
 ];
 
 // Types of decison trees:
 // - Categorical variable decision tree
 // - Continuous variable decision tree
 $(document).ready(() => {
-    for (const row of dataset) {
-        let tr: string = "";
-        for (const cell of row) {
-            tr += `<td>${cell}</td>`;
-        }
-        $("#tblDataset").append($(`<tr>${tr}</tr>`));
-    }
-    const decisionTree: Leaf | DecisionTree = DecisionTree.buildTree(dataset, 5);
-    $("#divDecisionTree").append(DecisionTree.printTree(decisionTree));
+	for (const row of dataset) {
+		let tr: string = "";
+		for (const cell of row) {
+			tr += `<td>${cell}</td>`;
+		}
+		$("#tblDataset").append($(`<tr>${tr}</tr>`));
+	}
+	const decisionTree: Leaf | DecisionTree = DecisionTree.buildTree(dataset, 5);
+	$("#divDecisionTree").append(DecisionTree.printTree(decisionTree));
 });
