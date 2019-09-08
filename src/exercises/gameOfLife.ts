56127d12c9ba43df66cd4d184eeba3154a618c66
ctapus <ctapus@hotmail.com>
2019-09-09 00:16:42 +0200

    gameOfLife work in progress

--- src/exercises/gameOfLife.ts
+++ src/exercises/gameOfLife.ts
@@ -1,12 +1,16 @@
 ﻿type PrintCellDelegate = (row: number, column: number) => void;
 class Game {
 	private cells: number[][];
-	public constructor(rows: number, columns: number) {
+	/**
+	 * @density
+	 * Probability that a cell is alive. Between 0 and 1.
+	 */
+	public constructor(rows: number, columns: number, density: number = 0) {
 		this.cells = [];
 		for (let r: number = 0; r < rows; r++) {
 			this.cells[r] = [];
-			for (let c: number; c < columns; c++) {
-				this.cells[r][c] = 0;
+			for (let c: number = 0; c < columns; c++) {
+				this.cells[r][c] = Math.random() < density ? 1 : 0;
 			}
 		}
 	}
@@ -22,6 +26,62 @@ class Game {
 			}
 		}
 	}
+	public heartbeat(): void {
+		const newCells: number[][] = [];
+		for (let r: number = 0; r < this.cells.length; r++) {
+			newCells[r] = [];
+			for (let c: number = 0; c < this.cells[r].length; c++) {
+				const neighbours: number[] = this.getCellNeighbours(r, c);
+				const aliveNeighbours = neighbours.reduce((acc, cur) => acc + cur);
+				newCells[r][c] = 0;
+				if (this.cells[r][c] === 1) {
+					// Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+					if (aliveNeighbours < 2) {
+						newCells[r][c] = 0;
+					}
+					// Any live cell with two or three live neighbours lives on to the next generation.
+					if (aliveNeighbours === 2 || aliveNeighbours === 3) {
+						newCells[r][c] = 1;
+					}
+					// Any live cell with more than three live neighbours dies, as if by overpopulation.
+					if (aliveNeighbours > 3) {
+						newCells[r][c] = 0;
+					}
+				} else {
+					// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+					if (aliveNeighbours === 3) {
+						newCells[r][c] = 1;
+					}
+				}
+			}
+		}
+		this.cells = newCells;
+	}
+	private getCellNeighbours(row: number, column: number): number[] {
+		let ret: number[] = [];
+		let rows: number[] = [row];
+		let columns: number[] = [column];
+		if (row > 0) {
+			rows = rows.concat(row - 1);
+		}
+		if (row < this.cells.length) {
+			rows = rows.concat(row + 1);
+		}
+		if (column > 0) {
+			columns = columns.concat(column - 1);
+		}
+		if (column < this.cells[0].length) {
+			columns = columns.concat(column + 1);
+		}
+		for (const r of rows) {
+			for (const c of columns) {
+				if (r !== row || c !== column) {
+					ret = ret.concat(this.cells[r][c]);
+				}
+			}
+		}
+		return ret;
+	}
 }
 
 $(document).ready(() => {
@@ -29,18 +89,25 @@ $(document).ready(() => {
 	const ctx: CanvasRenderingContext2D = canvas.getContext("2d");
 	const canvasWidth: number = canvas.width;
 	const canvasHeight: number = canvas.height;
-	const originX: number = canvasWidth / 2;
-	const originY: number = canvasHeight / 2;
 	const orthonormalGridLinesColor: string = "#f0f0ff";
-	const unitSize: number = 20;
+	const cellFillStyle: string = "#000000";
+	const unitSize: number = 10;
 	$("#legend").append($("<div></div>").text("Canvas size: " + canvasWidth + " x " + canvasHeight + " px"));
-	$("#legend").append($("<div></div>").text("Canvas origin: " + originX + " x " + originY + " px"));
 	drawOrthonormalGrid();
-	const game: Game = new Game(30, 30);
-	game.addCell(2, 3);
-	game.addCell(8, 3);
-	game.addCell(2, 15);
+	let game: Game = new Game(canvasHeight / unitSize, canvasWidth / unitSize, 0.3);
 	game.printGame(drawCell);
+	$("#btnRun").click(() => {
+		$("#btnRun").hide();
+		const interval: number = window.setInterval(() => {
+			game.heartbeat();
+			game.printGame(drawCell);
+		}, 500);
+	});
+	$("#btnNew").click(() => {
+		game = new Game(canvasHeight / unitSize, canvasWidth / unitSize, 0.3);
+		game.printGame(drawCell);
+		$("#btnRun").show();
+	});
 
 	function drawOrthonormalGrid(): void {
 		// grid
@@ -48,31 +115,19 @@ $(document).ready(() => {
 		ctx.strokeStyle = orthonormalGridLinesColor;
 		ctx.setLineDash([2, 2]);
 		ctx.lineWidth = 1;
-		let i: number = 0;
-		do {
-			ctx.moveTo(originX + unitSize * i, 0);
-			ctx.lineTo(originX + unitSize * i, canvasHeight);
+		for (let x: number = 0; x < canvasWidth; x += unitSize) {
+			ctx.moveTo(x, 0);
+			ctx.lineTo(x, canvasHeight);
 			ctx.stroke();
-			ctx.moveTo(originX - unitSize * i, 0);
-			ctx.lineTo(originX - unitSize * i, canvasHeight);
-			ctx.stroke();
-			i++;
 		}
-		while (originX + unitSize * i < canvasWidth);
-		i = 0;
-		do {
-			ctx.moveTo(0, originY + unitSize * i);
-			ctx.lineTo(canvasWidth, originY + unitSize * i);
+		for (let y: number = 0; y < canvasHeight; y += unitSize) {
+			ctx.moveTo(0, y);
+			ctx.lineTo(canvasWidth, y);
 			ctx.stroke();
-			ctx.moveTo(0, originY - unitSize * i);
-			ctx.lineTo(canvasWidth, originY - unitSize * i);
-			ctx.stroke();
-			i++;
 		}
-		while (originY + unitSize * i < canvasHeight);
 	}
 	function drawCell(row: number, column: number): void {
-		ctx.fillStyle = "#000000";
-		ctx.fillRect(column * unitSize, row * unitSize, unitSize - 2, unitSize - 2);
+		ctx.fillStyle = cellFillStyle;
+		ctx.fillRect(column * unitSize + 1, row * unitSize + 1, unitSize - 2, unitSize - 2);
 	}
 });
